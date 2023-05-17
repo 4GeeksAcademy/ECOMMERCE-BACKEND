085e2869ecf3978fe3925e17c199163dade0d92c
Benjamin Vilaplana Stark <120397795+BenBungle@users.noreply.github.com>
2023-05-17 01:36:17 +0000

    benja

--- src/app.py
+++ src/app.py
@@ -10,16 +10,19 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Order, Hamburger, Cheeseburger, VeggieBurger, Beverage, Acompañamientos, OnionRings, FrenchFries
 from flask_login import login_required
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import datetime
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 MIGRATE = Migrate(app, db)
@@ -141,6 +144,27 @@ def french_fries():
     return jsonify(french_fries_json)
 #rutas por crear: CREATE HAMBURGER, CRREATE BEBIDA, CREATE ACOMPAÑAMIENTO.
 # PLUS: CREAR POSTRES
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    body = request.get_json()
+    user = User.query.filter_by(email=body['email']).first
+    if not user:
+        new_user = User(email=body['email'], password=body['password'], is_active=True)
+        db.session.add(new_user)
+        db.session.commit()
+        expire =  datetime.timedelta(minutes=1)
+        new_token = create_access_token(identity=new_user.email,expires_delta=expire)
+        return jsonify({
+                "msg":"User was created",
+                "token":new_token,
+                "exp":expire.total_seconds()
+            })
+    else:
+        return jsonify({"msg":"The email entered already has an associated account. Please Log in"})
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
