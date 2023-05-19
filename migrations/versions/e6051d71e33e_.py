a9df8c6f13c7605bc3740e5e5adb2d9340648e78
MGcode23 <marcrudo23@gmail.com>
2023-05-19 20:58:41 +0000

    actualizada la rama mg

--- migrations/versions/e6051d71e33e_.py
+++ migrations/versions/e6051d71e33e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 04cbf250c08d
+Revision ID: e6051d71e33e
 Revises: 
-Create Date: 2023-05-17 01:27:24.183247
+Create Date: 2023-05-19 20:57:46.368233
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '04cbf250c08d'
+revision = 'e6051d71e33e'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/models.py
+++ src/models.py
@@ -4,13 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
+    is_admin = db.Column(db.Boolean(), default=False)
+
 
     def __repr__(self):
         return f'<User {self.id}: {self.name}>'
@@ -22,6 +25,7 @@ class User(db.Model):
             'email': self.email,
             'is_admin': self.is_admin
         }
+    
 class Hamburger(db.Model):
     __tablename__ = 'hamburgers'
     id = db.Column(db.Integer, primary_key=True)
@@ -119,3 +123,7 @@ class Order(db.Model):
             'quantity': self.quantity,
             'created_at': self.created_at
         }
+
+#alta cohesion y acoplamiento
+#principio de atomicidad
+#lo mas indivisible posible perfect
